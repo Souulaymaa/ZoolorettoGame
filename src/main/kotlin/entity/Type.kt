b0d9823bfc091f34cb92ee4d4987880250b5f61e
salma.dhimene <salma.dhimene@tu-dortmund.de>
2022-06-21 13:02:21 +0200

    fixed the names conventions of the enum classes

--- src/main/kotlin/entity/Type.kt
+++ src/main/kotlin/entity/Type.kt
@@ -2,14 +2,26 @@ package entity
 
 /**
  * Enum to distinguish between the 4 possible genders of the animals in Zooloretto:
- * weiblich, männlich, neutral and offspring
+ * female, male, neutral and offspring
  */
 
 enum class Type {
-    m,
-    w,
-    none,
-    offspring,
+    MALE,
+    FEMALE,
+    NONE,
+    OFFSPRING,
     ;
 
+
+    /**
+     * provide a single character to represent this value.
+     * Returns one of: M/F/N/O
+     */
+    override fun toString() =
+        when (this) {
+            Type.MALE -> "M"
+            Type.FEMALE -> "F"
+            Type.NONE -> "N"
+            Type.OFFSPRING -> "O"
+        }
 }
