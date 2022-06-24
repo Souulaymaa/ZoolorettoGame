3a4f28bc54fe2bf4667894b960fea9b85b16bdf6
alina.novosolova <alina.novosolova@tu-dortmund.de>
2022-06-24 12:51:45 +0200

    tileStack comments from Kassem corrected

--- src/test/kotlin/entity/TileStackTest.kt
+++ src/test/kotlin/entity/TileStackTest.kt
@@ -7,32 +7,33 @@ import kotlin.test.assertEquals
 
 /**
  * Test class for [TileStack].
- * Test with empty stack, right amount of tiles in the drawStack, wrong amount of tiles in endStack
  */
 class TileStackTest {
 
     /**
-     * Some tiles for adding to the different stacks
+     * Some tiles for tests
      */
     private val tileStack = TileStack(Stack<Tile>(), Stack<Tile>())
-    private val Animal1 = Animal(Type.FEMALE, Species.F)
-    private val Animal2 = Animal(Type.MALE, Species.K)
-    private val Animal3 = Animal(Type.OFFSPRING, Species.P)
-    private val Coin1 = Coin()
-    private val Coin2 = Coin()
+    private val tileStackTooManyTiles = TileStack(Stack<Tile>(), Stack<Tile>())
+    private val animal1 = Animal(Type.FEMALE, Species.F)
+    private val animal2 = Animal(Type.MALE, Species.K)
+    private val animal3 = Animal(Type.OFFSPRING, Species.P)
+    private val coin1 = Coin()
+    private val coin2 = Coin()
 
 
 
     /**
-     * Function to test the constructor of [TileStack] with correct arguments
+     * Function to load tiles in the drawStack and endStack
+     * to test the constructor of [TileStack] with correct arguments
      */
     @Test
     fun testConstructorCorrect(){
-        tileStack.drawStack.add(Animal1)
-        tileStack.drawStack.add(Animal2)
-        tileStack.drawStack.add(Animal3)
-        tileStack.endStack.add(Coin1)
-        tileStack.endStack.add(Coin2)
+        tileStack.drawStack.add(animal1)
+        tileStack.drawStack.add(animal2)
+        tileStack.drawStack.add(animal3)
+        tileStack.endStack.add(coin1)
+        tileStack.endStack.add(coin2)
 
 
         assertEquals(tileStack.drawStack.size, 3)
@@ -42,28 +43,12 @@ class TileStackTest {
     /**
      * Function to test the constructor of [TileStack] with incorrect argument
      */
-    /*@Test
+    @Test
     fun testConstructorIncorrect(){
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.endStack.add(Animal3)
-        tileStackTooMany.drawStack.add(Coin1)
-        tileStackTooMany.drawStack.add(Coin2)
+        for(i in 0..19){
+            tileStackTooManyTiles.endStack.add(coin1)
+        }
+        assertFalse{ tileStackTooManyTiles.endStack.size <= 15 }
+    }
 
-        assertFailsWith<IllegalArgumentException> {tileStackTooMany}
-    }*/
 }
