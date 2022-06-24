f2783d3e13e83debbe869598d4720d6d195b8837
Kassem Abbas <kassem_abbas6@outlook.com>
2022-06-24 15:21:15 +0200

    fixed the constuctor parameters and some documentation conventions

--- src/test/kotlin/entity/ZoolorettoGameStateTest.kt
+++ src/test/kotlin/entity/ZoolorettoGameStateTest.kt
@@ -24,12 +24,11 @@ class ZoolorettoGameStateTest {
     private val enclosure1 = Enclosure(6, 1, 0, Pair(4,3), false)
     private val enclosure2 = Enclosure(5, 2, 3, Pair(5,4), false)
     private val enclosure3 = Enclosure(4, 2, 2, Pair(6,3), false)
-    private val barn = Enclosure(100, 100, 0, Pair(0,0), true)
     private val playerEnclosures = mutableListOf(enclosure1, enclosure2, enclosure3)
 
     // Player Queue is also needed for the constructor of ZoolorettoGameState.
-    private val player1 = Player("Player 1", Difficulty.HUMAN, playerEnclosures, barn)
-    private val player2 = Player("Player 2", Difficulty.HUMAN, playerEnclosures, barn)
+    private val player1 = Player("Player 1", Difficulty.HUMAN)
+    private val player2 = Player("Player 2", Difficulty.HUMAN)
     private val players: Queue<Player> = LinkedList()
 
     /**
@@ -42,12 +41,14 @@ class ZoolorettoGameStateTest {
         deliveryTrucks.add(deliveryTruck1)
         deliveryTrucks.add(deliveryTruck2)
         deliveryTrucks.add(deliveryTruck3)
-
+        player1.playerEnclosure = playerEnclosures
         players.add(player1)
         players.add(player2)
 
-        val zoolorettoGameState = ZoolorettoGameState(players = players, tileStack = tileStack,
-            deliveryTrucks = deliveryTrucks)
+        val zoolorettoGameState = ZoolorettoGameState(
+            players = players, tileStack = tileStack,
+            deliveryTrucks = deliveryTrucks
+        )
 
         assertNotNull(zoolorettoGameState)
 
