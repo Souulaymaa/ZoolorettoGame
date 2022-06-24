05f8d152177cfb6ff879f85c13a8a424ea43d00a
sanad marji <sanad.marji@tu-dortmund.de>
2022-06-24 12:48:52 +0200

    VendingStallTest.kt completed

--- src/test/kotlin/entity/VendingStallTest.kt
+++ src/test/kotlin/entity/VendingStallTest.kt
@@ -3,11 +3,17 @@ package entity
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+/**
+ * test class for VendingStall
+ */
 internal class VendingStallTest{
 
     private val testCase1 = VendingStall(StallType.VENDING1)
     private val testCase2 = VendingStall(StallType.VENDING2)
 
+    /**
+     * checks functionality of constructor
+     */
     @Test
     fun checkObject(){
         assertEquals(testCase1.stall, StallType.VENDING1)
