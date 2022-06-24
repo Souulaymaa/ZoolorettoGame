9c574f13732a24fd861902271ecb15b6b9fd9f48
M <M>
2022-06-24 23:13:48 +0200

    implemented deepZoolorettoCopy

--- src/main/kotlin/service/GameStateService.kt
+++ src/main/kotlin/service/GameStateService.kt
@@ -1,11 +1,23 @@
 package service
 
+import com.google.gson.Gson
 import entity.ZoolorettoGameState
 
 class GameStateService {
 
 }
 
+/**
+ * Function to create a deep copy of [ZoolorettoGameState] object without
+ * any references to the object's properties.
+ *
+ * @param toCopy Object, which to copy from
+ * @return A copy of the Parameter
+ */
 fun deepZoolorettoCopy(toCopy : ZoolorettoGameState) : ZoolorettoGameState {
-    throw NotImplementedError()
+    val gson = Gson()
+    val jsonString = gson.toJson(toCopy) // Serialization
+    val gameCopy = gson.fromJson(jsonString, ZoolorettoGameState::class.java) // Deserialization
+
+    return gameCopy
 }
