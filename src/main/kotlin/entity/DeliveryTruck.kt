9f1542efd3045d0bb2e525bf76192c1e8a374a9e
sanad marji <sanad.marji@tu-dortmund.de>
2022-06-24 12:47:37 +0200

    DeliveryTruckTest commented and DeliveryTruck init removed

--- src/main/kotlin/entity/DeliveryTruck.kt
+++ src/main/kotlin/entity/DeliveryTruck.kt
@@ -4,13 +4,8 @@ package entity
  * Class represents the DeliveryTrack
  *
  * @param size has the fixe value 3
- * @param tilesOnTruck is an Array with 0..3 Tiles
  */
 data class DeliveryTruck(val size: Int = 3) {
 
     val tilesOnTruck = arrayListOf<Tile>()
-
-    init {
-        require(tilesOnTruck.size in 0..3) { "Size of tiles must be 0-3. Size was " + tilesOnTruck.size }
-    }
 }
--- src/test/kotlin/entity/DeliveryTruckTest.kt
+++ src/test/kotlin/entity/DeliveryTruckTest.kt
@@ -4,30 +4,27 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFails
 
+/**
+ * DeliveryTruck Test class
+ */
 internal class DeliveryTruckTest{
 
     private val failedTruck = DeliveryTruck()
     private val stall = VendingStall(StallType.VENDING1)
     private val animal1 = Animal(Type.FEMALE, Species.S)
     private val animal2 = Animal(Type.FEMALE, Species.S)
-    private val coin = Coin()
 
     /**
-     currently not possible:
-
-    fun testException(){
-    assertFails {
-    failedTruck.tilesOnTruck.add(stall)
-    failedTruck.tilesOnTruck.add(animal1)
-    failedTruck.tilesOnTruck.add(animal2)
-    failedTruck.tilesOnTruck.add(coin) }
-    }
+     * function to test constructor functionality
+     * and if tiles are added to tilesOnTruck arraylist properly
      */
-
-
     @Test
     fun checkObject(){
         assertEquals(failedTruck.size, 3)
+        failedTruck.tilesOnTruck.add(stall)
+        failedTruck.tilesOnTruck.add(animal1)
+        failedTruck.tilesOnTruck.add(animal2)
+        assertEquals(3, failedTruck.tilesOnTruck.size)
     }
 
 }
