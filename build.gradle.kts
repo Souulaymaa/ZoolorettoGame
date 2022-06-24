b2f5463002f9a14ac00c93a2b80c99b9ce146146
M <M>
2022-06-24 23:07:07 +0200

    Added Gson dependency to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ application {
 dependencies {
     testImplementation(kotlin("test-junit5"))
     implementation(group = "tools.aqua", name = "bgw-gui", version = "0.6")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
 
 tasks.distZip {
