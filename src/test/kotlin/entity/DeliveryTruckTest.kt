a79a56eae05d4314da83351045c6ebb9e9064880
sanad marji <sanad.marji@tu-dortmund.de>
2022-06-28 12:03:02 +0200

    DeliveryTruckTest.kt pushed again

--- src/test/kotlin/entity/DeliveryTruckTest.kt
+++ src/test/kotlin/entity/DeliveryTruckTest.kt
@@ -2,29 +2,28 @@ package entity
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.test.assertFails
 
 /**
  * DeliveryTruck Test class
  */
 internal class DeliveryTruckTest{
 
-    private val failedTruck = DeliveryTruck()
+    private val successTruck = DeliveryTruck()
     private val stall = VendingStall(StallType.VENDING1)
     private val animal1 = Animal(Type.FEMALE, Species.S)
     private val animal2 = Animal(Type.FEMALE, Species.S)
 
     /**
-     * function to test constructor functionality
+     * function to test constructor initialization
      * and if tiles are added to tilesOnTruck arraylist properly
      */
     @Test
     fun checkObject(){
-        assertEquals(failedTruck.size, 3)
-        failedTruck.tilesOnTruck.add(stall)
-        failedTruck.tilesOnTruck.add(animal1)
-        failedTruck.tilesOnTruck.add(animal2)
-        assertEquals(3, failedTruck.tilesOnTruck.size)
+        assertEquals(successTruck.size, 3)
+        successTruck.tilesOnTruck.add(stall)
+        successTruck.tilesOnTruck.add(animal1)
+        successTruck.tilesOnTruck.add(animal2)
+        assertEquals(3, successTruck.tilesOnTruck.size)
     }
 
 }
