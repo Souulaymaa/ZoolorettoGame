c1251e7ef13bbbe1c93b494351ae0e2d2d63dae2
Kassem Abbas <kassem_abbas6@outlook.com>
2022-06-30 13:24:23 +0200

    fixed some more ide warnings

--- src/main/kotlin/gameAI/moves/AddTileToTruck.kt
+++ src/main/kotlin/gameAI/moves/AddTileToTruck.kt
@@ -3,14 +3,15 @@ package gameAI.moves
 import entity.DeliveryTruck
 import gameAI.Move
 
-class AddTileToTruck(val truck: DeliveryTruck) : Move {
+class AddTileToTruck(private val truck: DeliveryTruck) : Move {
 
     override fun performMove() {
         super.performMove()
     }
 
     override fun toHintString(): String {
-        return "Add drawn Tile to Truck ${truck.truckNumber}."
+        //To be completed
+        return "Add drawn Tile to Truck."
     }
 
 }
