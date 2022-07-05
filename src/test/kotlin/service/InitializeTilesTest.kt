e3a75ce7724991b94544e70d8ecad8b9e5999c28
Saskia <saskia.ortmann@tu-dortmund.de>
2022-07-05 19:33:44 +0200

    Test auf Größen des draw und endStacks und wirft Exception wenn Offspring im Stapel ist

--- src/test/kotlin/service/InitializeTilesTest.kt
+++ src/test/kotlin/service/InitializeTilesTest.kt
@@ -2,91 +2,42 @@ package service
 
 import entity.*
 import org.junit.jupiter.api.Test
-import java.util.*
 import kotlin.test.assertEquals
 
 class InitializeTilesTest {
 
     private val p1 = Player("p1", Difficulty.HUMAN)
     private val p2 = Player("p2", Difficulty.HUMAN)
-    private val p3 = Player("p3", Difficulty.HUMAN)
-    private val activePlayers: Queue<Player> = LinkedList(listOf(p1, p2))
     private val activePlayer: List<Player> = listOf(p1, p2)
-    private val tileStack = TileStack(Stack<Tile>(), Stack<Tile>())
-    private val deliveryTrucks = mutableListOf<DeliveryTruck>(
-        DeliveryTruck(3),
-        DeliveryTruck(3),
-        DeliveryTruck(3))
+
 
     /**
-     * Test cases for [initialiseTiles] and [initialiseTileStack]
+     * Test cases for initialiseTiles and initialiseTileStack
      */
     @Test
     fun initializeTileStackTest(){
         val rootService = RootService()
-        //println(game.currentGameState.players.size)
         val zoolorettoGameService = rootService.zoolorettoGameService
-        //println(zoolorettoGameService.players.size)
-        println(zoolorettoGameService.coins.size)
         zoolorettoGameService.createZoolorettoGame(activePlayer, false)
-        println(zoolorettoGameService.coins.size)
         val game = rootService.zoolorettoGame!!.currentGameState
 
-        //println(game.players.size)
+        //nur Ausgabe der Tiles
+        //println(game.tileStack.drawStack)
 
-        var counter = 0
+        val speciesList = listOf(Species.Z, Species.S, Species.U, Species.P, Species.L, Species.K, Species.F, Species.E)
         for(t in game.tileStack.drawStack){
-            if(t.equals(Species.E)){
-                counter++
-                break
+            for(s in speciesList){
+                if(t.equals(Animal(type = Type.OFFSPRING, species = s, hasChild = false))){
+                    throw java.lang.IllegalArgumentException("Type = Offspring!")
+                }
             }
         }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.F)){
-                counter++
-                break
-            }
-        }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.K)){
-                counter++
-                break
-            }
-        }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.L)){
-                counter++
-                break
-            }
-        }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.P)){
-                counter++
-                break
-            }
-        }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.S)){
-                counter++
-                break
-            }
-        }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.U)){
-                counter++
-                break
-            }
-        }
-        for(t in game.tileStack.drawStack){
-            if(t.equals(Species.Z)){
-                counter++
-                break
-            }
-        }
+
         assertEquals(2, game.players.size)
-        assertEquals(88, game.tileStack.drawStack.size)
-        assertEquals(6, counter)
+        assertEquals(72, game.tileStack.drawStack.size)
         assertEquals(15, game.tileStack.endStack.size)
 
     }
+
 }
+
